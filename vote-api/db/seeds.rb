a59e57a9b36361a98c23dacbe8eb0689ee49e4f4
Angegter <92314600+Angegter@users.noreply.github.com>
2025-12-10 13:34:24 +0200

    Update seeds.rb

--- vote-api/db/seeds.rb
+++ vote-api/db/seeds.rb
@@ -1,4 +1,16 @@
-poll = Poll.create!(title: "What is your favorite language?")
-poll.votes.create!(option: "Ruby")
-poll.votes.create!(option: "JavaScript")
-poll.votes.create!(option: "Python")
+poll1 = Poll.create!(title: "Your favorite programming language?") 
+poll1.votes.create!(option: "Ruby")
+poll1.votes.create!(option: "JavaScript")
+poll1.votes.create!(option: "Python")
+
+poll2 = Poll.create!(title: "Best frontend framework?")
+poll2.votes.create!(option: "React")
+poll2.votes.create!(option: "Vue")
+poll2.votes.create!(option: "Angular")
+
+poll3 = Poll.create!(title: "Which day is best for deployment?")
+poll3.votes.create!(option: "Monday")
+poll3.votes.create!(option: "Friday")
+poll3.votes.create!(option: "Saturday")
+
+puts "Created #{Poll.count} polls with initial votes."
